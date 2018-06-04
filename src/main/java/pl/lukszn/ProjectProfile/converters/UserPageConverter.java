e1e441d2ae3784c5c4bd509f08a3a437c3de7d44
Lukszn <lukaszsznurowski@gmail.com>
2018-06-04 15:46:46 +0200

    UserPageConverter -fix first problem

--- src/main/java/pl/lukszn/ProjectProfile/converters/UserPageConverter.java
+++ src/main/java/pl/lukszn/ProjectProfile/converters/UserPageConverter.java
@@ -1,5 +1,19 @@
 package pl.lukszn.ProjectProfile.converters;
 
-public class UserPageConverter {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
 
+import pl.lukszn.ProjectProfile.models.UserPageModel;
+import pl.lukszn.ProjectProfile.repositories.UserPageRepository;
+
+
+public class UserPageConverter  implements Converter<String, UserPageModel>{
+	
+	@Autowired
+	private UserPageRepository userPageRepository;
+	
+	public UserPageModel convert(String source) {
+		UserPageModel userPageModel = userPageRepository.findOne(Long.parseLong(source));
+		return userPageModel;
+	}
 }
