262d22f0bf0a51e7cfc82c3a03a75ff794cee3d3
Lukszn <lukaszsznurowski@gmail.com>
2018-06-07 13:28:50 +0200

    changing AccountController

--- src/main/java/pl/lukszn/ProjectProfile/controllers/AccountController.java
+++ src/main/java/pl/lukszn/ProjectProfile/controllers/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pl.lukszn.ProjectProfile.models.User;
 import pl.lukszn.ProjectProfile.models.Account;
 import pl.lukszn.ProjectProfile.repositories.AccountRepository;
 import pl.lukszn.ProjectProfile.repositories.UserRepository;
@@ -25,15 +26,18 @@ public class AccountController {
 	UserRepository userRepository;
 	
 	
-	@RequestMapping(method = RequestMethod.GET, value ="accounts/addAccount")
+	@RequestMapping(method = RequestMethod.GET, value ="addAccount")
 	public String addAccount(Model model) {
 		Account account = new Account();
 		model.addAttribute("account", account);
+		
 		return "addAccount";
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value ="accounts/addAccount")
-	public String addAccount(@ModelAttribute Account account) {
+	@RequestMapping(method = RequestMethod.POST, value ="addAccount")
+	public String addAccount(@ModelAttribute Account account, HttpSession session) {
+		User user = userRepository.findOne((Long) session.getAttribute("user_id"));
+		account.setUser(user);
 		accountRepository.save(account);
 		return "redirect:/accounts";
 	}
--- src/main/webapp/WEB-INF/views/addAccount.jsp
+++ src/main/webapp/WEB-INF/views/addAccount.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,5 +9,19 @@
 </head>
 <body>
 
+<form:form method="post" modelAttribute="account">
+    
+		<h1>Create an Account</h1>
+		
+		Title: 
+		<form:input path="accTitle" placeholder="Title" required="required" class="input pass"/>
+	
+		Description:
+		<form:input path="accDescription" placeholder="Description" required="required" class="input pass"/>
+      
+        
+        <input type="submit" value="Add Account" class="inputButton" id="signUpButton"/>  
+          
+	</form:form>
 </body>
 </html>
